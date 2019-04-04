f99907f5342b9ba536ff5454a8a70505cb22a1af
novak <kelvin.onkundi@andela.com>
2019-04-04 15:05:48 +0300

    updated stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
-//entry point of the application
-const path = require('path') 
+// entry point of the application
+const path = require('path')
 
-module.exports = {
+module.exports = env => {
+  const isProduction = env === 'production'
+  return {
     entry: './src/app.js',
     output: {
-        path: path.join(__dirname, 'public'), //absolute path on your machine to where you wanna output the output file
-        filename: 'bundle.js'
+      path: path.join(__dirname, 'public'), // absolute path on your machine to where you wanna output the output file
+      filename: 'bundle.js'
     },
-    module:{
-        rules:[{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        }
+      ]
     },
-    devtool: 'cheap-module-eval-source-map',
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'public')
+      contentBase: path.join(__dirname, 'public')
     }
-};
+  }
+}
